23f4f350e2f1be9915cf0c69117077270e578f0c
Florent Ortiz <florent.ortiz@capgemini.com>
2023-03-03 10:39:02 +0100

    Modif address space en dure le temps de trouver la synthaxe

--- azure_vnet.tf
+++ azure_vnet.tf
@@ -3,5 +3,5 @@ resource "azurerm_virtual_network" "GROUPE-A-VNET-TF" {
   name                = var.azure_vnet_name
   resource_group_name = var.azure_resource_group_name
   location            = var.azure_resource_group_location
-  address_space       = [var.azure_vnet_name]
+  address_space       = ["10.0.0.0/16"]
 }
