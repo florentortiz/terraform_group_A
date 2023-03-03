79a971f51f78c4cff41c35a0dd8841c861f11db4
afef0409 <afef.ben-benslimen@capgemini.com>
2023-03-03 09:54:33 +0100

    ressource group

--- ressource_group.tf
+++ ressource_group.tf
@@ -1,19 +1,5 @@
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-
-  subscription_id = "a177ac1b-f0e1-45e5-a6c4-80266ff85e1d"
-}
 
 # Create a resource group
 resource "azurerm_resource_group" "GROUPE-A-RG-TF" {
