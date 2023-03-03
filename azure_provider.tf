5866c816d21949da45fa8e441713f400baced011
Florent Ortiz <florent.ortiz@capgemini.com>
2023-03-03 09:25:21 +0100

    Add azure_provider file for our subscription

---
+++ azure_provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  # subscription_id : "Ecole By Cap"
+  subscription_id = "a177ac1b-f0e1-45e5-a6c4-80266ff85e1d"
+}
