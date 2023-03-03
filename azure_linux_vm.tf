b702ef749391ad2d572e8a7ac92907ce2559a2a3
Florent Ortiz <florent.ortiz@capgemini.com>
2023-03-03 11:49:17 +0100

    Ajout VM FO

--- azure_linux_vm.tf
+++ azure_linux_vm.tf
@@ -12,6 +12,17 @@ resource "azurerm_network_interface" "test-vm-groupe-a-nic" {
   }
 }
 
+resource "azurerm_network_interface" "test-vm-groupe-a-nic-fo" {
+  name                = "${var.vm_name_pfx}-nic"
+  location            = var.azure_resource_group_location
+  resource_group_name = var.azure_resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.GROUPE-A-SUBNET-TF.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
 #create vm
 resource "azurerm_linux_virtual_machine" "test-vm-groupe-a" {
@@ -32,6 +43,37 @@ resource "azurerm_linux_virtual_machine" "test-vm-groupe-a" {
 
   os_disk {
     name                 = "my-terraform-os-disk-${var.vm_name_pfx}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+ 
+}
+
+resource "azurerm_linux_virtual_machine" "test-vm-groupe-a-fo" {
+  name                            = "${var.vm_name_pfx}-fo"
+  resource_group_name             = var.azure_resource_group_name
+  location                        = var.azure_resource_group_location
+  size                            = "Standard_B1s" #allowed: Standard_B1ls Standard_B1ms Standard_B1s Standard_B2s Standard_D2s_v3 Standard_DS1_v2
+  computer_name                   = "myvm-fo"
+  admin_username                  = "adminuser"
+  admin_password                  = "Password1234!"
+  disable_password_authentication = false
+
+
+ network_interface_ids = [
+    azurerm_network_interface.test-vm-groupe-a-nic-fo.id,
+  ]
+ 
+
+  os_disk {
+    name                 = "my-terraform-os-disk-${var.vm_name_pfx}-fo"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
