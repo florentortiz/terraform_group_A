124f74cf5b8e43a0dc55b35a30f65e574db90d91
Florent Ortiz <florent.ortiz@capgemini.com>
2023-03-03 11:31:34 +0100

    Ajout subnet

--- azure_vnet.tf
+++ azure_vnet.tf
@@ -5,3 +5,10 @@ resource "azurerm_virtual_network" "GROUPE-A-VNET-TF" {
   location            = var.azure_resource_group_location
   address_space       = ["10.0.0.0/16"]
 }
+
+resource "azurerm_subnet" "GROUPE-A-SUBNET-TF" {
+  name                 = "internal"
+  resource_group_name  = var.azure_resource_group_name
+  virtual_network_name = var.azure_vnet_name
+  address_prefixes     = ["10.0.6.0/24"] # to change by Groupe ....
+}
