c3ac99531ef6953db739e3f4d5bd155cfd4e8ea8
afef0409 <afef.ben-benslimen@capgemini.com>
2023-03-03 10:21:07 +0100

    create file vnet

--- azure_ressource_group.tf
+++ azure_ressource_group.tf
@@ -1,16 +1,7 @@
 
-
-
 # Create a resource group
 resource "azurerm_resource_group" "GROUPE-A-RG-TF" {
   name     = "GROUPE-A-RG-TF"
   location = "West Europe"
 }
 
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "GROUPE-A-VNET-TF" {
-  name                = "GROUPE-A-VNET-TF"
-  resource_group_name = azurerm_resource_group.GROUPE-A-RG-TF.name
-  location            = azurerm_resource_group.GROUPE-A-RG-TF.location
-  address_space       = ["10.0.0.0/16"]
-}
