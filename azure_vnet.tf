84567b9bd03798f79001e599834444ad444d8dc6
Florent Ortiz <florent.ortiz@capgemini.com>
2023-03-03 11:35:17 +0100

    Passage suvnet name en variable

--- azure_vnet.tf
+++ azure_vnet.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "GROUPE-A-VNET-TF" {
 }
 
 resource "azurerm_subnet" "GROUPE-A-SUBNET-TF" {
-  name                 = "internal"
+  name                 = var.azure_subnet_name
   resource_group_name  = var.azure_resource_group_name
   virtual_network_name = var.azure_vnet_name
   address_prefixes     = ["10.0.6.0/24"] # to change by Groupe ....
